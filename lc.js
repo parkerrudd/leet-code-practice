3b2ac320cf324205e9b665e9a4226bf815981b5c
Parker Rudd <parkerbrudd@gmail.com>
2022-04-29 12:26:41 -0600

    Implement strStr() solution

--- lc.js
+++ lc.js
@@ -1,12 +1,5 @@
-var removeElement = function(nums, val) {
-    let indexCounter = 0; 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != val) {
-            nums[indexCounter] = nums[i]
-            indexCounter++; 
-        }
-    }; 
-    return indexCounter;
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle); 
 };
 
-console.log(removeElement([0,1,2,2,3,0,4,2], 2)); 
+console.log(strStr("aaaaa", "bba")); 
