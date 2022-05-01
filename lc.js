984bf78dbddb3dc57a1ff3eacb3b138b2688ada2
Parker Rudd <parkerbrudd@gmail.com>
2022-05-01 11:23:06 -0600

    search insert position

--- lc.js
+++ lc.js
@@ -1,5 +1,13 @@
-var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle); 
+var searchInsert = function(nums, target) {
+  if (nums.includes(target)) {
+    return nums.indexOf(target); 
+  } else if (!nums.includes(target)) {
+    nums.push(target) 
+    nums.sort((a,b) => {
+      return a - b; 
+    }) 
+    return nums.indexOf(target); 
+  }
 };
 
-console.log(strStr("aaaaa", "bba")); 
+console.log(searchInsert([1,3,5,6], 2)); 
