b8f29b413517966fcd74698b6690f7d38e163b60
Parker Rudd <parkerbrudd@gmail.com>
2022-05-05 15:59:32 -0600

    Rank Transform of an Array

--- lc.js
+++ lc.js
@@ -1,12 +1,15 @@
-var targetIndices = function(nums, target) {
-  let sorted = nums.sort((a, b) => a - b); 
-  let res = []; 
+var arrayRankTransform = function(arr) {
+  //sort arr from in increasing order
+  const sorted = [...arr].sort((a, b) => a - b);
+  //remove duplicate values from array 
+  const finalSort = []; 
   for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] === target) {
-      res.push(i)
+    if (!finalSort.includes(sorted[i])) {
+      finalSort.push(sorted[i])
     }
   }
-  return res; 
+  //find the index of each value from arr in finalSort && add one to begin rank at 1
+  return arr.map((x) => finalSort.indexOf(x) + 1);
 };
 
-console.log(targetIndices([1,2,5,2,3], 2)); 
+console.log(arrayRankTransform([40,10,20,30,10])); 
