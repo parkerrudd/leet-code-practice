6fd50f0cf36f3fbb644590e1ad5efd1a53cbc88f
Parker Rudd <parkerbrudd@gmail.com>
2022-05-04 22:52:24 -0600

    find target indicies after sorting array

--- lc.js
+++ lc.js
@@ -1,18 +1,12 @@
-var mostFrequent = function(nums, key) {
-  let count = []; 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i - 1] === key) count.push(nums[i]); 
-  }
-  let obj = {}; 
-  for (let i of count) {
-    if (obj[i]) {
-      obj[i]++
-    } else {
-      obj[i] = 1
+var targetIndices = function(nums, target) {
+  let sorted = nums.sort((a, b) => a - b); 
+  let res = []; 
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === target) {
+      res.push(i)
     }
   }
-  let sorted = Object.keys(obj).sort((a, b) => obj[b] - obj[a]); 
-  return sorted[0];
+  return res; 
 };
 
-console.log(mostFrequent([2,2,2,2,3], 2)); 
+console.log(targetIndices([1,2,5,2,3], 2)); 
