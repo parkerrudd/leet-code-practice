7afab880b12e177cca4d8f130297fd49edfcb649
Parker Rudd <parkerbrudd@gmail.com>
2022-06-08 12:44:15 -0600

    linked lists

--- lc.js
+++ lc.js
@@ -1,15 +1,101 @@
-var arrayRankTransform = function(arr) {
-  //sort arr from in increasing order
-  const sorted = [...arr].sort((a, b) => a - b);
-  //remove duplicate values from array 
-  const finalSort = []; 
-  for (let i = 0; i < sorted.length; i++) {
-    if (!finalSort.includes(sorted[i])) {
-      finalSort.push(sorted[i])
-    }
-  }
-  //map through original array to find out when they appear in finalSort array & add one to begin rank at 1
-  return arr.map((x) => finalSort.indexOf(x) + 1);
-};
+class LinkedList {
+   constructor() {
+      this.head = this.tail = null
+   }
+   //add to end of list / tail
+   append(value) {
+      //if list is empty
+      if (!this.tail) {
+         this.head = this.tail = new Node(value)
+      } else {
+         let oldTail = this.tail
+         this.tail = new Node(value)
+         oldTail.next = this.tail
+         this.tail.prev = oldTail
+      }
+   }
 
-console.log(arrayRankTransform([40,10,20,30,10])); 
+   //add to beginning of list / head
+   prepend(value) {
+      //if list is empty 
+      if (!this.head) {
+         this.head = this.tail = new Node(value)
+      } else {
+         let oldHead = this.head
+         this.head = new Node(value)
+         oldHead.prev = this.head
+         this.head.next = oldHead
+      }
+   }
+
+   deleteHead() {
+      //if list is empty 
+      if (!this.head) {
+         return null
+      } else {
+         let removedHead = this.head
+         // if 1 node left 
+         if (this.head === this.tail) {
+            this.head = this.tail = null
+         } else {
+            this.head = this.head.next
+            this.head.prev = null
+         }
+         return removedHead.value
+      }
+   }
+
+   deleteTail() {
+      //if list is empty 
+      if (!this.tail) {
+         return null
+      } else {
+         let removedTail = this.tail
+         //if 1 node left
+         if (this.tail === this.head) {
+            this.tail = this.head = null
+         } else {
+            this.tail = this.tail.prev
+            this.tail.next = null 
+         }
+
+         return removedTail.value
+      }
+
+   }
+
+   search(value) {
+      let currentNode = this.head
+
+      while (currentNode) {
+         if (currentNode.value === value) {
+            return currentNode
+         }
+         currentNode = currentNode.next
+      }
+
+      return null
+   }
+}
+
+class Node {
+   constructor(value, prev, next) {
+      this.value = value
+      this.prev = prev || null
+      this.next = next || null
+   }
+}
+
+let list = new LinkedList()
+
+list.append(1)
+list.append(2)
+list.append(3)
+
+list.prepend(0)
+list.prepend(-1)
+
+list.deleteHead()
+list.deleteTail()
+
+console.log(list);
