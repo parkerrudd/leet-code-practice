6fb059db69e348fa74938a58109f5219f254c467
Parker Rudd <parkerbrudd@gmail.com>
2022-06-12 14:05:58 -0600

    queues

--- lc.js
+++ lc.js
@@ -1,32 +1,29 @@
-class Stack {
+class Queue {
    constructor() {
       this.storage = {}
-      this.size = 0
+      this.head = 0
+      this.tail = 0
    }
 
-   push(element) {
-      this.size++
-      this.storage[this.size] = element
+   enqueue(element) {
+      this.storage[this.tail] = element
+      this.tail++
    }
 
-   pop() {
-      let removed = this.storage[this.size]
-      delete this.storage[this.size]
-      this.size--
+   dequeue(){
+      let removed = this.storage[this.head]
+      delete this.storage[this.head]
+      this.head++
       return removed
-   }
-
-   peek() {
-      return this.storage[this.size]
    }
 }
 
-const stack = new Stack
+const queue = new Queue 
 
-stack.push('dog')
-stack.push('cat')
-stack.push('bear')
-stack.pop()
+queue.enqueue('seahorse')
+queue.enqueue('dolphin')
+queue.enqueue('whale shark')
 
+queue.dequeue()
 
-console.log(stack)
+console.log(queue)
