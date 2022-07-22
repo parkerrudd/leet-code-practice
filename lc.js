528863d008694fafb97f53e061ccb475fc143be5
Parker Rudd <parkerbrudd@gmail.com>
2022-07-22 13:40:14 -0600

    optimized hash map solve for valid anagrams

--- lc.js
+++ lc.js
@@ -1,24 +1,18 @@
 var isAnagram = function(s, t) {
    if (s.length !== t.length) return false
-   let first = s.split('').sort().join('')
-   let second = t.split('').sort().join('')
-   let freq1 = {}
-   let freq2 = {}
-   for (let i in first) {
-      if (freq1[first[i]]) {
-         freq1[first[i]]++
-      } else {
-         freq1[first[i]] = 1
-      }
+   const freq1 = {}
+   const freq2 = {}
+   const length = s.length
+   for (let i = 0; i < length; i++) {
+      if (!freq1[s[i]]) freq1[s[i]] = 0
+      if (!freq2[t[i]]) freq2[t[i]] = 0
+      freq1[s[i]]++
+      freq2[t[i]]++
    }
-   for (let i in second) {
-      if (freq2[second[i]]) {
-         freq2[second[i]]++
-      } else {
-         freq2[second[i]] = 1
-      }
+   for (let ch in freq1) {
+      if (freq1[ch] !== freq2[ch]) return false
    }
-   return JSON.stringify(freq1) === JSON.stringify(freq2) 
+   return true
 };
 
-console.log(isAnagram('car', 'rat'))
+console.log(isAnagram('car', 'rac'))
