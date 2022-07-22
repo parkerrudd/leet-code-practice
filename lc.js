341eafcf479306dc7fdde6628662f90dce3e4f49
Parker Rudd <parkerbrudd@gmail.com>
2022-07-22 13:59:17 -0600

    find Kth largest

--- lc.js
+++ lc.js
@@ -1,5 +1,6 @@
-const firstAndLast = (nums, target) => {
-   return [nums.indexOf(target), nums.lastIndexOf(target)]
-}
+var findKthLargest = function(nums, k) {
+   nums.sort((a, b) => b - a)
+   return nums[k - 1]
+};
 
-console.log(firstAndLast([5,7,7,8,8,10], 8))
+console.log(findKthLargest([3,2,1,5,6,4], 2));
