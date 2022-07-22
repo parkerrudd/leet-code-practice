8c205ae90c017080c83659cea6788001d016f4d2
Parker Rudd <parkerbrudd@gmail.com>
2022-07-22 13:47:31 -0600

    first and last

--- lc.js
+++ lc.js
@@ -1,18 +1,5 @@
-var isAnagram = function(s, t) {
-   if (s.length !== t.length) return false
-   const freq1 = {}
-   const freq2 = {}
-   const length = s.length
-   for (let i = 0; i < length; i++) {
-      if (!freq1[s[i]]) freq1[s[i]] = 0
-      if (!freq2[t[i]]) freq2[t[i]] = 0
-      freq1[s[i]]++
-      freq2[t[i]]++
-   }
-   for (let ch in freq1) {
-      if (freq1[ch] !== freq2[ch]) return false
-   }
-   return true
-};
+const firstAndLast = (nums, target) => {
+   return [nums.indexOf(target), nums.lastIndexOf(target)]
+}
 
-console.log(isAnagram('car', 'rac'))
+console.log(firstAndLast([5,7,7,8,8,10], 8))
