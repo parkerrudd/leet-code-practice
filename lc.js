825a24857a351d6adf3f4e859aad147b4f26b0c3
Parker Rudd <parkerbrudd@gmail.com>
2022-08-01 20:07:53 -0600

    merge two lists

--- lc.js
+++ lc.js
@@ -1,16 +1,23 @@
-const divisorSubstrings = (num, k) => {
-    let currNum = []
-    let count = 0
-    let numsArr = num.toString().split('')
-    for (let i = 0; i < numsArr.length; i++) {
-        currNum.push(numsArr[i])
-        let current = currNum.join('')
-        if (i >= k - 1) {
-            if (num % current === 0) count++
-            currNum.shift()   
-         }
-      }
-   return count
+const mergeTwoLists = function(list1, list2) {
+   const dummy = new ListNode(-Infinity)
+   let prev = dummy
+   
+   while (list1 && list2) {
+       if (list1.val <= list2.val) {
+           prev.next = list1
+           prev = list1
+           list1 = list1.next
+       } else {
+           prev.next = list2
+           prev = list2
+           list2 = list2.next
+       }
+   }
+   
+   if (!list1) prev.next = list2
+   if (!list2) prev.next = list1
+
+   return dummy.next
 };
 
-console.log(divisorSubstrings(430043, 2));
+console.log(mergeTwoLists([1,2,4], [1,3,4]));
