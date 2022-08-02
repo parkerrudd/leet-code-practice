34d2b592c301ac42e89fb94c9564f33d842c613e
Parker Rudd <parkerbrudd@gmail.com>
2022-08-02 14:42:39 -0600

    mySqrt (binary search)

--- lc.js
+++ lc.js
@@ -1,21 +1,40 @@
-var searchInsert = function(nums, target) {
-   let lower = 0
-   let upper = nums.length - 1
+// var searchInsert = function(nums, target) {
+//    let lower = 0
+//    let upper = nums.length - 1
    
-   while (lower < upper) {
+//    while (lower < upper) {
+//        const middle = lower + Math.floor((upper - lower) / 2)
+       
+//        if (nums[middle] === target) return middle
+       
+//        if (nums[middle] < target) {
+//            lower = middle + 1
+//        }
+       
+//        if (nums[middle] > target) {
+//            upper = middle - 1
+//        }
+//    }
+   
+//    return -1
+   
+// };
+
+var mySqrt = function(x) {
+   let lower = 1
+   let upper = x 
+   while (lower <= upper) {
        const middle = lower + Math.floor((upper - lower) / 2)
+       console.log(middle);
+       if (Math.round(middle * middle) === x) return middle
        
-       if (nums[middle] === target) return middle
+       if (Math.round(middle * middle) > x) upper = middle - 1
        
-       if (nums[middle] < target) {
-           lower = middle + 1
-       }
-       
-       if (nums[middle] > target) {
-           upper = middle - 1
-       }
+       if (Math.round(middle * middle) < x) lower = middle + 1
    }
-   
-   return -1
-   
+   return lower + Math.floor((upper - lower) / 2)
 };
+
+console.log(mySqrt(25));
+
+1,2,3,4,5,6,7,8
