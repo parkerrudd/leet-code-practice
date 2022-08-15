fbcf39d439ecb88a029ae3f79bb5f407febcee8b
Parker Rudd <parkerbrudd@gmail.com>
2022-08-14 19:23:36 -0600

    queue with array

--- lc.js
+++ lc.js
@@ -1,40 +1,31 @@
-// var searchInsert = function(nums, target) {
-//    let lower = 0
-//    let upper = nums.length - 1
-   
-//    while (lower < upper) {
-//        const middle = lower + Math.floor((upper - lower) / 2)
-       
-//        if (nums[middle] === target) return middle
-       
-//        if (nums[middle] < target) {
-//            lower = middle + 1
-//        }
-       
-//        if (nums[middle] > target) {
-//            upper = middle - 1
-//        }
-//    }
-   
-//    return -1
-   
-// };
+class Queue {
+    constructor() {
+        this.items = []
+    }
 
-var mySqrt = function(x) {
-   let lower = 1
-   let upper = x 
-   while (lower <= upper) {
-       const middle = lower + Math.floor((upper - lower) / 2)
-       console.log(middle);
-       if (Math.round(middle * middle) === x) return middle
-       
-       if (Math.round(middle * middle) > x) upper = middle - 1
-       
-       if (Math.round(middle * middle) < x) lower = middle + 1
-   }
-   return lower + Math.floor((upper - lower) / 2)
-};
+    enqueue(element) {
+        this.items.unshift(element)
+    }
 
-console.log(mySqrt(25));
+    dequeue() {
+        let removed = this.items.pop()
+        return removed
+    }
 
-1,2,3,4,5,6,7,8
+    isEmpty() {
+        return this.items.length === 0
+    }
+
+    front() {
+        if (this.items.length === 0) return null
+        return this.items[this.items.length - 1]
+    }
+}
+
+const queue = new Queue 
+
+queue.enqueue('dolphin')
+queue.enqueue('whale')
+queue.enqueue('orca')
+
+console.log(queue);
